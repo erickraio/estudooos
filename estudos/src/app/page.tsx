79253ca09e7c471e56599d917a6a4d1580d2c96e
erickraio <erickleandroabade.03@gmail.com>
2025-10-24 16:58:31 -0300

    commitando apenas a page, nao eh pra mudar o layout

--- estudos/src/app/page.tsx
+++ estudos/src/app/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p>testando a pagina</p>
+      <h1>Comandos importantes para o Git</h1>
+      <p>git add .</p>
+      <p>git commit -m "#Mensagem"</p>
+      <p>git push origin</p>
+      <p>git pull </p>
+      <p>git branch "#NomeDaBranch"</p>
+      <p>git switch "#NomeDaBranch"</p>
+      <p>oiiii</p>
     </div>
   );
 }
